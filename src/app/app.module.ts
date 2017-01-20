aa0dcf6e5c867605a57ca9e0ca75b00c6cb998b6
Timo <timo.hasenbichler@gmail.com>
2017-01-20 23:00:44 +0100

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
